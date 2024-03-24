a10eaa9afe43449a80ac327c01939930e8f6bf81
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-24 17:32:04 +0100

    Type object

--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
+// const person:
+// { name: string; age: number }
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
-console.log(person);
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
