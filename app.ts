8ac64c698232943270ef23d7043a0dc046026471
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-26 21:30:34 +0100

    Classes

--- app.ts
+++ app.ts
@@ -1,10 +1,33 @@
 class Department {
   name: string;
+  employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department("Accounting");
 console.log(accounting);
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Bitch");
+accounting.describe();
+accounting.printEmployeeInfo();
+
+// const accountingCopy = { name: "s", describe: accounting.describe };
+
+// accountingCopy.describe();
