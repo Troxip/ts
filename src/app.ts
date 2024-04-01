76d2e67f74a268b2edbbe798378dcd29bc81c6e8
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-01 18:02:29 +0200

    Annoy

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,26 @@
 //   data.split(" ");
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 // console.log(merge({ name: "Max" }, { age: 21 }));
-const mergedObj = merge({ name: "Max" }, { age: 21 });
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
