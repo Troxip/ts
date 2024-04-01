b28a2655384107b9141612f657ea2b0f7a30d698
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-01 18:50:00 +0200

    Stupid

--- src/app.ts
+++ src/app.ts
@@ -34,3 +34,43 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends Object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+const objStorage = new DataStorage<object>();
+objStorage.addItem({ name: "Max" });
+objStorage.addItem({ name: "Manu" });
+
+objStorage.removeItem({ name: "Manu" });
+console.log(objStorage.getItems());
